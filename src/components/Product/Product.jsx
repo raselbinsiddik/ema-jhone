4bea8412fcdaa28cff6189d02cac6dc18d70ac3c
Muhammad Rasel Bin Siddik <muhammadraselbinsiddik@gmail.com>
2023-03-27 23:10:51 +0600

    first commit

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import './Product.css';
+
+const Product = (props) => {
+    const { img, name, seller, price, ratings } = props.product;
+
+   const handleAddToCart = props.handleAddToCart;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <h6 className='product-name'>{name}</h6>
+                <p>Price: ${price}</p>
+                <p>Manufacture: {seller}</p>
+                <p>Rating: {ratings} Stars</p>
+            </div>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add to cart <FontAwesomeIcon icon={faShoppingCart} />
+</button>
+        </div>
+    );
+};
+
+export default Product;
