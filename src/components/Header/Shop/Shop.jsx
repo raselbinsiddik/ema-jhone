4f3010a572fb69b9372dc5e0c8e4065618d7353f
Muhammad Rasel Bin Siddik <muhammadraselbinsiddik@gmail.com>
2023-03-28 00:40:29 +0600

    add

--- src/components/Header/Shop/Shop.jsx
+++ src/components/Header/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../../Cart/Cart';
 import Product from '../../Product/Product';
 import './Shop.css';
 const Shop = () => {
@@ -25,9 +26,8 @@ const Shop = () => {
                }
             </div>
             <div className='cart-container'>
-                <h2>order summary</h2>
-                <p>Selected cart:{cart.length }</p>
-            </div> 
+                <Cart cart={cart}></Cart>
+            </div>   
         </div>
     );
 };
